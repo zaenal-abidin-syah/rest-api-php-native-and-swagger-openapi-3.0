6503f3005dc40ef2d8fa5078a8057bec5f79631b
zaenal-abidin-syah <aszaenal618@gmail.com>
2025-03-12 06:34:21 +0700

    fixes cors

--- index.php
+++ index.php
@@ -1,6 +1,11 @@
 <?php
 
 // Atur header
+
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
+header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json");
 $userJson = "users.json";
 $users = array(
